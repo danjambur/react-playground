76a30c5f9518b7c5640b00d21a34922cf0b8fa63
Danny Burnett <danny.burnett@yardlink.com>
2023-07-08 14:34:03 +0100

    Sets up component types / set up

--- src/components/InfiniteLoader/index.tsx
+++ src/components/InfiniteLoader/index.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 
-const InfiniteLoader = () => {
+interface InfiniteLoaderProps<T> {
+  apiEndpoint: string; // any api endpoint
+  page: number; // pagination page
+  limit: number; // pagination limit
+  renderContent: (data: T[]) => React.ReactNode;
+}
+
+function InfiniteLoader<T>({
+  apiEndpoint,
+  page,
+  limit,
+  renderContent,
+}: InfiniteLoaderProps<T>) {
   return (
     <div>
       <h1>InfiniteLoader</h1>
     </div>
   );
-};
+}
 
 export default InfiniteLoader;
