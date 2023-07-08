5c031f94fe8cc853d75e98a39f713b9d05aa67ce
Danny Burnett <danny.burnett@yardlink.com>
2023-07-08 14:37:52 +0100

    Set up the basis for fetching universally

--- src/components/InfiniteLoader/index.tsx
+++ src/components/InfiniteLoader/index.tsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 interface InfiniteLoaderProps<T> {
   apiEndpoint: string; // any api endpoint
-  page: number; // pagination page
   limit: number; // pagination limit
   renderContent: (data: T[]) => React.ReactNode;
 }
 
 function InfiniteLoader<T>({
   apiEndpoint,
-  page,
   limit,
   renderContent,
 }: InfiniteLoaderProps<T>) {
+  const [data, setData] = React.useState<T[]>([]);
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [page, setPage] = React.useState<number>(1);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`${apiEndpoint}?page=${page}&limit=${limit}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setData((prevData) => [...prevData, ...res.data]);
+        setPage((prevPage) => prevPage + 1);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoading(false));
+  }, [apiEndpoint, page, limit]);
+
   return (
     <div>
       <h1>InfiniteLoader</h1>
