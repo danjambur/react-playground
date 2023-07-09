81dd5f52ea9291faa7139ac2531813bf0c1cc604
Danny Burnett <danny.burnett@yardlink.com>
2023-07-09 11:04:39 +0100

    Update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,17 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+window.IntersectionObserver = jest.fn(function (
+  this: IntersectionObserver,
+  callback: IntersectionObserverCallback,
+  options?: IntersectionObserverInit
+) {
+  this.observe = jest.fn();
+  return this;
+});
+
+test("renders Infinite Loader Component", () => {
   render(<App />);
-  const linkElement = screen.getByText(/InfiniteLoader/i);
+  const linkElement = screen.getByTestId("infinite_loader");
   expect(linkElement).toBeInTheDocument();
 });
