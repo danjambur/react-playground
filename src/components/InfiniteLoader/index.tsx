0990b4e78b8aaafa3a370009a7351a2da09a8900
Danny Burnett <danny.burnett@yardlink.com>
2023-07-09 16:21:36 +0100

    refactor to use react-intersection-observer

--- src/components/InfiniteLoader/index.tsx
+++ src/components/InfiniteLoader/index.tsx
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect, useCallback } from "react";
 import { useInfiniteQuery } from "react-query";
+import { useInView } from "react-intersection-observer";
 
 interface InfiniteLoaderProps<T> {
   apiEndpoint: string; // any api endpoint
@@ -12,52 +13,58 @@ function InfiniteLoader<T>({
   limit,
   renderContent,
 }: InfiniteLoaderProps<T>) {
-  const loaderRef = useRef(null);
+  const { ref, inView } = useInView();
   const [pageParam, setPageParam] = React.useState(1);
 
-  const { data, isLoading, isError, fetchNextPage, hasNextPage } =
-    useInfiniteQuery("data", () => fetchMoreData(), {
-      getNextPageParam: (_lastPage, allPages) => {
-        const nextPage = allPages.length + 1;
-        return nextPage;
-      },
-    });
+  const {
+    data,
+    isLoading,
+    isError,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery("data", () => fetchMoreData(), {
+    getNextPageParam: (_lastPage, allPages) => {
+      const nextPage = allPages.length + 1;
+      return nextPage;
+    },
+  });
 
   const fetchMoreData = async () => {
+    // we make the assumption here that the API will have both pages and a limit in order to paginate requests
     const res = await fetch(`${apiEndpoint}?page=${pageParam}&limit=${limit}`);
     const data = await res.json();
     setPageParam((prev) => prev + 1);
     return data;
   };
 
-  const handleObserver = useCallback(
-    (entries: [any]) => {
-      const [target] = entries;
-      if (target.isIntersecting) {
-        fetchNextPage();
-      }
-    },
-    [fetchNextPage, hasNextPage]
-  );
-
   useEffect(() => {
-    const element = loaderRef.current;
-    const option = { threshold: 0 };
-
-    const observer = new IntersectionObserver(handleObserver, option);
-    observer.observe(element);
-    return () => observer.unobserve(element);
-  }, [fetchNextPage, hasNextPage, handleObserver]);
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView]);
 
   return (
-    <div>
-      {data?.pages.map((page, i) => (
-        <React.Fragment key={i}>{renderContent(page)}</React.Fragment>
-      ))}
-      {isLoading && <div>Loading...</div>}
-      {isError && <div>Error fetching data</div>}
-      <div ref={loaderRef} className="h-px opacity-0 pointer-events-none" />
-    </div>
+    <>
+      <>
+        {data?.pages.map((page) => renderContent(page))}
+        {isLoading && <div>Loading...</div>}
+        {isError && <div>Error fetching data</div>}
+      </>
+      <button
+        ref={ref}
+        onClick={() => fetchNextPage()}
+        disabled={!hasNextPage || isFetchingNextPage}
+        type="button"
+        className="rounded bg-indigo-600 px-2 py-1 text-xs font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        {isFetchingNextPage
+          ? "Loading more..."
+          : hasNextPage
+          ? "Load Newer"
+          : "Nothing more to load"}
+      </button>
+    </>
   );
 }
 
